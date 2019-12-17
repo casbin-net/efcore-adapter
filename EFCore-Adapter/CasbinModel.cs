f4d10d5f42dd20289c154771d298c68219032869
arthuridea <arthuridea@gmail.com>
2019-12-17 12:57:54 +0800

    add generic typed model support

--- EFCore-Adapter/CasbinModel.cs
+++ EFCore-Adapter/CasbinModel.cs
@@ -1,22 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Casbin.NET.Adapter.EFCore
 {
-    public partial class CasbinDbContext : DbContext
+    public partial class CasbinDbContext<TKey> : DbContext
+        where TKey : IEquatable<TKey>
     {
-        public virtual DbSet<CasbinRule> CasbinRule { get; set; }
+        public virtual DbSet<CasbinRule<TKey>> CasbinRule { get; set; }
 
-        private readonly IEntityTypeConfiguration<CasbinRule> _casbinModelConfig;
+        private readonly IEntityTypeConfiguration<CasbinRule<TKey>> _casbinModelConfig;
 
         public CasbinDbContext()
         {
         }
 
-        public CasbinDbContext(DbContextOptions<CasbinDbContext> options) : base(options)
+        public CasbinDbContext(DbContextOptions<CasbinDbContext<TKey>> options) : base(options)
         {
         }
 
-        public CasbinDbContext(DbContextOptions<CasbinDbContext> options, IEntityTypeConfiguration<CasbinRule> casbinModelConfig) : base(options)
+        public CasbinDbContext(DbContextOptions<CasbinDbContext<TKey>> options, IEntityTypeConfiguration<CasbinRule<TKey>> casbinModelConfig) : base(options)
         {
             _casbinModelConfig = casbinModelConfig;
         }
@@ -31,9 +33,10 @@ namespace Casbin.NET.Adapter.EFCore
 
     }
 
-    public partial class CasbinRule
+    public partial class CasbinRule<TKey> : ICasbinRule
+        where TKey : IEquatable<TKey>
     {
-        public int Id { get; set; }
+        public virtual TKey Id { get; set; }
         public string PType { get; set; }
         public string V0 { get; set; }
         public string V1 { get; set; }
@@ -42,5 +45,16 @@ namespace Casbin.NET.Adapter.EFCore
         public string V4 { get; set; }
         public string V5 { get; set; }
 
+    }
+
+    public interface ICasbinRule
+    {
+        string PType { get; set; }
+        string V0 { get; set; }
+        string V1 { get; set; }
+        string V2 { get; set; }
+        string V3 { get; set; }
+        string V4 { get; set; }
+        string V5 { get; set; }
     }
 }
