32f667439bfea4b03b18768cbf0df48a69272977
Sagilio <Sagilio@outlook.com>
2020-09-20 18:11:09 +0800

    Improve extendibility of adapter
    Signed-off-by: Sagilio <Sagilio@outlook.com>

---
+++ EFCore-Adapter/CasbinModelExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using NetCasbin.Model;
+using NetCasbin.Persist;
+
+namespace Casbin.NET.Adapter.EFCore
+{
+    public static class CasbinModelExtension
+    {
+        internal static void LoadPolicyFromCasbinRules<TCasbinRule>(this Model casbinModel, IEnumerable<TCasbinRule> rules) 
+            where TCasbinRule : class, ICasbinRule
+        {
+            foreach (TCasbinRule rule in rules)
+            {
+                string ruleString = rule.ConvertToString();
+                Helper.LoadPolicyLine(ruleString, casbinModel);
+            }
+        }
+
+    }
+}
