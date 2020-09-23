c76c050ff46a0e2b9a34bd20931d2bb6ea608786
Sagilio <Sagilio@outlook.com>
2020-09-23 15:19:28 +0800

    Improve extendibility of adapter (#27)

--- EFCore-Adapter/CasbinDbContext.cs
+++ EFCore-Adapter/CasbinDbContext.cs
@@ -31,28 +31,4 @@ namespace Casbin.NET.Adapter.EFCore
         }
 
     }
-
-    public partial class CasbinRule<TKey> : ICasbinRule where TKey : IEquatable<TKey>
-    {
-        public virtual TKey Id { get; set; }
-        public string PType { get; set; }
-        public string V0 { get; set; }
-        public string V1 { get; set; }
-        public string V2 { get; set; }
-        public string V3 { get; set; }
-        public string V4 { get; set; }
-        public string V5 { get; set; }
-
-    }
-
-    public interface ICasbinRule
-    {
-        string PType { get; set; }
-        string V0 { get; set; }
-        string V1 { get; set; }
-        string V2 { get; set; }
-        string V3 { get; set; }
-        string V4 { get; set; }
-        string V5 { get; set; }
-    }
 }
