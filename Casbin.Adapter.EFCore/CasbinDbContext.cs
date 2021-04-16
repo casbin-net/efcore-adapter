4fe3c5ca67a62d8f03c1bb7247f3660c86a540de
Sagilio <42855245+Sagilio@users.noreply.github.com>
2021-04-16 16:12:14 +0800

    feat: Add non generic DbContextOptions support (#44)
    Signed-off-by: Sagilio <Sagilio@outlook.com>

--- Casbin.Adapter.EFCore/CasbinDbContext.cs
+++ Casbin.Adapter.EFCore/CasbinDbContext.cs
@@ -15,6 +15,18 @@ namespace Casbin.Adapter.EFCore
         {
         }
 
+        public CasbinDbContext(DbContextOptions options, string defaultSchemaName = null, string defaultTableName = "casbin_rule") : base(options)
+        {
+            _casbinModelConfig = new DefaultCasbinRuleEntityTypeConfiguration<TKey>(defaultTableName);
+            _defaultSchemaName = defaultSchemaName;
+        }
+
+        public CasbinDbContext(DbContextOptions options, IEntityTypeConfiguration<CasbinRule<TKey>> casbinModelConfig, string defaultSchemaName = null) : base(options)
+        {
+            _casbinModelConfig = casbinModelConfig;
+            _defaultSchemaName = defaultSchemaName;
+        }
+
         public CasbinDbContext(DbContextOptions<CasbinDbContext<TKey>> options, string defaultSchemaName = null, string defaultTableName = "casbin_rule") : base(options)
         {
             _casbinModelConfig = new DefaultCasbinRuleEntityTypeConfiguration<TKey>(defaultTableName);
