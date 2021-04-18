3d309f9f2b2cc8e90c791ce03c7ca979eefcb133
Sagilio <Sagilio@outlook.com>
2021-04-18 19:32:08 +0800

    fix: Unable resolve CasbinDbContext
    Signed-off-by: Sagilio <Sagilio@outlook.com>

---
+++ Casbin.Adapter.EFCore.UnitTest/DependencyInjectionTest.cs
@@ -0,0 +1,32 @@
+﻿using Casbin.Adapter.EFCore.UnitTest.Fixtures;
+using Microsoft.Extensions.DependencyInjection;
+using NetCasbin.Persist;
+using Xunit;
+
+namespace Casbin.Adapter.EFCore.UnitTest
+{
+    public class DependencyInjectionTest : IClassFixture<TestHostFixture>
+    {
+        private readonly TestHostFixture _testHostFixture;
+
+        public DependencyInjectionTest(TestHostFixture testHostFixture)
+        {
+            _testHostFixture = testHostFixture;
+        }
+
+        [Fact]
+        public void ShouldResolveCasbinDbContext()
+        {
+            var dbContext = _testHostFixture.Services.GetService<CasbinDbContext<int>>();
+            Assert.NotNull(dbContext);
+            dbContext.Database.EnsureCreated();
+        }
+
+        [Fact]
+        public void ShouldResolveEfCoreAdapter()
+        {
+            var adapter = _testHostFixture.Services.GetService<IAdapter>();
+            Assert.NotNull(adapter);
+        }
+    }
+}
