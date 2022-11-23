4b711e7ff6019a83d91129d7f04a0d9755126db2
sagilio <sagilio@outlook.com>
2022-11-23 10:42:05 +0800

    feat: Use new IAdapter interface
    BREAKING CHANGE: Use new IAdapter interface

---
+++ Casbin.Adapter.EFCore/Entities/EFCorePersistPolicy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Casbin.Persist;
+
+namespace Casbin.Adapter.EFCore.Entities
+{
+    public class EFCorePersistPolicy<TKey> : PersistPolicy, IEFCorePersistPolicy<TKey> where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
---
+++ Casbin.Adapter.EFCore/IPersistPolicy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Casbin.Persist;
+
+namespace Casbin.Adapter.EFCore
+{
+    public interface IEFCorePersistPolicy<TKey> : IPersistPolicy where TKey : IEquatable<TKey>
+    {
+        public TKey Id { get; set; }
+    }
+}
