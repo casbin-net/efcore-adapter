203269be80577e80c6d0c2a43671c75fcf2af4b4
sagilio <sagilio@outlook.com>
2023-08-20 21:51:52 +0800

    fix: Error configuration at default ctor of CasbinDbContext
    (cherry picked from commit b51e0e084e4afdeae48a65449e88f6e0bfdc9046)

--- Casbin.Persist.Adapter.EFCore/CasbinDbContext.cs
+++ Casbin.Persist.Adapter.EFCore/CasbinDbContext.cs
@@ -7,43 +7,45 @@ namespace Casbin.Persist.Adapter.EFCore
     public partial class CasbinDbContext<TKey> : DbContext where TKey : IEquatable<TKey>
     {
         public virtual DbSet<EFCorePersistPolicy<TKey>> Policies { get; set; }
+        private const string DefaultTableName = "casbin_rule";
 
         private readonly IEntityTypeConfiguration<EFCorePersistPolicy<TKey>> _casbinModelConfig;
-        private readonly string _defaultSchemaName;
+        private readonly string _schemaName;
 
         public CasbinDbContext()
         {
+            _casbinModelConfig = new DefaultPersistPolicyEntityTypeConfiguration<TKey>(DefaultTableName);
         }
 
-        public CasbinDbContext(DbContextOptions<CasbinDbContext<TKey>> options, string defaultSchemaName = null, string defaultTableName = "casbin_rule") : base(options)
+        public CasbinDbContext(DbContextOptions<CasbinDbContext<TKey>> options, string schemaName = null, string tableName = DefaultTableName) : base(options)
         {
-            _casbinModelConfig = new DefaultPersistPolicyEntityTypeConfiguration<TKey>(defaultTableName);
-            _defaultSchemaName = defaultSchemaName;
+            _casbinModelConfig = new DefaultPersistPolicyEntityTypeConfiguration<TKey>(tableName);
+            _schemaName = schemaName;
         }
-
-        public CasbinDbContext(DbContextOptions<CasbinDbContext<TKey>> options, IEntityTypeConfiguration<EFCorePersistPolicy<TKey>> casbinModelConfig, string defaultSchemaName = null) : base(options)
+        
+        protected CasbinDbContext(DbContextOptions options, string schemaName = null, string tableName = DefaultTableName) : base(options)
         {
-            _casbinModelConfig = casbinModelConfig;
-            _defaultSchemaName = defaultSchemaName;
+            _casbinModelConfig = new DefaultPersistPolicyEntityTypeConfiguration<TKey>(tableName);
+            _schemaName = schemaName;
         }
 
-        protected CasbinDbContext(DbContextOptions options, string defaultSchemaName = null, string defaultTableName = "casbin_rule") : base(options)
-        {
-            _casbinModelConfig = new DefaultPersistPolicyEntityTypeConfiguration<TKey>(defaultTableName);
-            _defaultSchemaName = defaultSchemaName;
-        }
-
-        protected CasbinDbContext(DbContextOptions options, IEntityTypeConfiguration<EFCorePersistPolicy<TKey>> casbinModelConfig, string defaultSchemaName = null) : base(options)
+        public CasbinDbContext(DbContextOptions<CasbinDbContext<TKey>> options, IEntityTypeConfiguration<EFCorePersistPolicy<TKey>> casbinModelConfig, string schemaName = null) : base(options)
         {
             _casbinModelConfig = casbinModelConfig;
-            _defaultSchemaName = defaultSchemaName;
+            _schemaName = schemaName;
+        }
+        
+        protected CasbinDbContext(DbContextOptions options, IEntityTypeConfiguration<EFCorePersistPolicy<TKey>> casbinModelConfig, string schemaName = null) : base(options)
+        {
+            _casbinModelConfig = casbinModelConfig;
+            _schemaName = schemaName;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            if (string.IsNullOrWhiteSpace(_defaultSchemaName) is false)
+            if (string.IsNullOrWhiteSpace(_schemaName) is false)
             {
-                modelBuilder.HasDefaultSchema(_defaultSchemaName);
+                modelBuilder.HasDefaultSchema(_schemaName);
             }
 
             if (_casbinModelConfig is not null)
